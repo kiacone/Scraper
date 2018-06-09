d26545ec9844b2170ecfcdb74d66beca06e4a831
Kristin Iacone <kiacone@gmail.com>
2018-06-09 14:22:09 -0400

    update server file

--- server.js
+++ server.js
@@ -27,12 +27,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+// mongoose.Promise = Promise;
+// mongoose.connect(MONGODB_URI);
 
 
 // Connect to the Mongo DB
